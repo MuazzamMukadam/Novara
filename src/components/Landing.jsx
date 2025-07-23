ed7156a93f65ec6bd0d1927ed2a3bc10f0354aa6
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-07-23 21:13:11 +0530

    s

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,38 +1,9 @@
-import { useEffect, useState } from 'react';
 import './Landing.css';
 
 function Landing() {
-  const [songs, setSongs] = useState([]);
-
-useEffect(() => {
-fetch('https://novara-4e6p.onrender.com/api/tracks')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Full response:", data);
-      setSongs(data);
-    });
-}, []);
 
   return (
-    <div className="lc">
-      {songs.map((song, idx) => {
-        return (
-          <a
-            href={song.external}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={idx}
-            className="card-link"
-          >
-            <div className="card">
-              <img src={song.image} alt={song.name} className="cover" />
-              <h3>{song.name}</h3>
-              <p>{song.artist}</p>
-            </div>
-          </a>
-        );
-      })}
-    </div>
+ <></>
   );
 }
 
