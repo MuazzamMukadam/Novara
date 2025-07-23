59cda6368463d23ae20a5e0822b86f740d068869
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-07-23 17:58:21 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import Landing from './components/Landing';
+import { useState, useEffect } from "react";
+import Introscreen from './components/Introscreen';
 function App() {
+  const [isIntroVisible, setIntroVisible] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIntroVisible(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer); // Cleanup
+  }, []);
 
   return (
-   <>
-   <Landing/>
-   </>
-  )
+    <>
+      {isIntroVisible ? <Introscreen /> : <Landing />}
+    </>
+  );
 }
-
-export default App
+export default App;
--- src/components/Introscreen.jsx
+++ src/components/Introscreen.jsx
@@ -1,13 +1,15 @@
 import './Introscreen.css';
 
-
-function Introscreen(){
+function Introscreen() {
     return (
         <>
         <div className="ic">
-            
+            <img src='NovaraLogo.jpeg' alt="Novara Logo" />
+            <h3>NOVARA</h3>
+            <p className="tagline">Music So Heavy, It Bends Time.</p>
         </div>
         </>
     );
 }
+
 export default Introscreen;
