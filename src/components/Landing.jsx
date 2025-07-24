b85281dbc15db728c0eff2f95192badc3ad90ac9
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-07-24 18:04:12 +0530

    s

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,49 +1,57 @@
-import './Landing.css';
-import Header from './Header';
-import Carousel from './Carousel';
-import { useState, lazy, Suspense } from 'react';
+import "./Landing.css";
+import Header from "./Header";
+import Carousel from "./Carousel";
+import { useState, lazy, Suspense } from "react";
 
-const LazyHappy = lazy(() => import('../moods/Happy'));
-const LazySad = lazy(() => import('../moods/Sad'));
-const LazyEnergetic = lazy(() => import('../moods/Energetic'));
-const LazyChill = lazy(() => import('../moods/Chill'));
-const LazyRomantic = lazy(() => import('../moods/Romantic'));
-const LazyMoody = lazy(() => import('../moods/Moody'));
-const LazyGif = lazy(() => import('../moods/Gif'));
+
+const LazyHappy = lazy(() => import("../moods/Happy"));
+const LazySad = lazy(() => import("../moods/Sad"));
+const LazyEnergetic = lazy(() => import("../moods/Energetic"));
+const LazyChill = lazy(() => import("../moods/Chill"));
+const LazyRomantic = lazy(() => import("../moods/Romantic"));
+const LazyMoody = lazy(() => import("../moods/Moody"));
+const LazyGif = lazy(() => import("../moods/Gif"));
 
 function Landing() {
-  const [selectedMood, setSelectedMood] = useState('choose');
+  const [selectedMood, setSelectedMood] = useState("choose");
 
   const renderMoodComponent = () => {
     switch (selectedMood) {
-      case 'choose':
+      case "choose":
         return <LazyGif />;
-      case 'happy':
+      case "happy":
         return <LazyHappy />;
-      case 'sad':
+      case "sad":
         return <LazySad />;
-      case 'energetic':
+      case "energetic":
         return <LazyEnergetic />;
-      case 'chill':
+      case "chill":
         return <LazyChill />;
-      case 'romantic':
+      case "romantic":
         return <LazyRomantic />;
-      case 'moody':
+      case "moody":
         return <LazyMoody />;
       default:
         return null;
     }
   };
+ 
+
 
   return (
-    <>
+    <> 
       <div className="top-center-blend"></div>
+     
       <Header />
-<h2 className='mtfu'>Music that feels you.</h2><br></br>
-<h4 className='suv'>Select your vibe. We'll set the mood.</h4>
-<Carousel/>
+
+      <h2 className="mtfu">Music that feels you.</h2>
+      <br></br>
+      <h4 className="suv">Select your vibe. We'll set the mood.</h4>
+      <Carousel />
       <div className="mood-container">
-        <label htmlFor="mood-select" className="mood-label">Select your mood:</label>
+        <label htmlFor="mood-select" className="mood-label">
+          Select your mood:
+        </label>
         <select
           id="mood-select"
           className="mood-dropdown"
--- src/moods/Sad.jsx
+++ src/moods/Sad.jsx
@@ -1,13 +1,25 @@
+import './Sad.css';
+
+const trackIds = [
+  "2pUpNOgJBIBCcjyQZQ00qU"
+];
+
 function Sad() {
   return (
-    <iframe
-      src="https://open.spotify.com/embed/track/7bPWdJgx8vek7S5i5yAtvG"
-      width="100%"
-      height="152"
-      frameBorder="0"
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-    ></iframe>
+    <>
+      {trackIds.map((id, index) => (
+        <iframe
+          key={index}
+          src={`https://open.spotify.com/embed/track/${id}`}
+          width="100%"
+          height="152"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ))}
+    </>
   );
 }
 
 export default Sad;
+
