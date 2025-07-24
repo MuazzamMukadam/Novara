606ccb31819e954ee6f4a204a81946654b67ff56
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-07-24 14:05:46 +0530

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import './Header.css';
-import { useState } from 'react';
+import "./Header.css";
+import { useState } from "react";
 
 function Header() {
   const [showAbout, setShowAbout] = useState(false);
@@ -13,9 +13,18 @@ function Header() {
         </div>
 
         <div className="right-icon" onClick={() => setShowAbout(true)}>
-          <svg className="info-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" viewBox="0 0 24 24">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10
-            10-4.48 10-10S17.52 2 12 2zm0 15h-1v-6h2v6h-1zm0-8h-1V7h2v2h-1z"/>
+          <svg
+            className="info-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="white"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10
+            10-4.48 10-10S17.52 2 12 2zm0 15h-1v-6h2v6h-1zm0-8h-1V7h2v2h-1z"
+            />
           </svg>
         </div>
       </header>
@@ -23,13 +32,28 @@ function Header() {
       {showAbout && (
         <div className="about-popup">
           <div className="about-content">
-            <button className="close-btn" onClick={() => setShowAbout(false)}>×</button>
-            <h2>About <span className='anv'>NOVARA</span></h2>
-            <p>Hey ! It's Muazzam . <br></br>Novara is a mood-based music discovery 
-            platform that lets users select how they feel
-             and instantly get curated songs to match. With a sleek, minimalist 
-             UI, Novara makes music selection fast, aesthetic, and vibe-driven. No sign-up walls, no fluff , just pure mood-matching tunes 
-             in one click. Songs redirect seamlessly to Spotify, so you can dive straight into the vibe. </p>
+            <button className="close-btn" onClick={() => setShowAbout(false)}>
+              ×
+            </button>
+            <h2>
+              About <span className="anv">NOVARA</span>
+            </h2>
+            <p>
+              Hey ! It's{" "}
+              <a
+                href="https://muazzammukadam.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="gradient-name">Muazzam</span>.
+              </a>
+              <br></br>Novara is a mood-based music discovery platform that lets
+              users select how they feel and instantly get curated songs to
+              match. With a sleek, minimalist UI, Novara makes music selection
+              fast, aesthetic, and vibe-driven. No sign-up walls, no fluff ,
+              just pure mood-matching tunes in one click. Songs redirect
+              seamlessly to Spotify, so you can dive straight into the vibe.{" "}
+            </p>
           </div>
         </div>
       )}
