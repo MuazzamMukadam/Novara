2425b1f82dad3ad4559a111cab8f10b01a40cd15
Muazzam Mukadam <muazzammukadam009@gmail.com>
2025-07-24 12:36:31 +0530

    s

--- src/moods/Energetic.jsx
+++ src/moods/Energetic.jsx
@@ -1,5 +1,16 @@
 function Energetic() {
-  return <div className="mood-card">You’re feeling Happy! 🎉</div>;
+    return(
+   <>
+<div className="romantic-container">
+      <iframe src="https://open.spotify.com/embed/track/0FIDCNYYjNvPVimz5icugS?utm_source=generator"
+    width="100%"
+    height="152"
+    frameborder="0"
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    >
+  </iframe>
+</div>
+    </>)
 }
 
 export default Energetic;
--- src/moods/Romantic.jsx
+++ src/moods/Romantic.jsx
@@ -1,5 +1,20 @@
+import './Romantic.css';
+
 function Romantic() {
-  return <div className="mood-card">You’re feeling Happy! 🎉</div>;
+return(
+    <>
+<div className="romantic-container">
+      <iframe src="https://open.spotify.com/embed/track/2262bWmqomIaJXwCRHr13j?utm_source=generator"
+    width="100%"
+    height="152"
+    frameborder="0"
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    >
+  </iframe>
+</div>
+    </>
+  )
 }
 
 export default Romantic;
+
